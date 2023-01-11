71166d3b69a1a9c65bccbb94892c8bca2b27ce9d
kristiansabols <kristineabeltina79@gmail.com>
2023-01-11 09:41:13 +0200

    Update funkcija.js

--- funkcija.js
+++ funkcija.js
@@ -3,15 +3,37 @@
 //Jāpārbauda ievadītais
 //Jāizvada atbilde
 
-let datoraSkaitlis=10
+let datoraSkaitlis=10;
+
 
 function nolasitIevadito(){
     let ievaditais = document.getElementById("ievade").value;
     console.log(ievaditais);
+    return ievaditais;
+}
+
+function salidzinatSkaitlus(cilvekaSkaitlis){
+    if (isNaN(cilvekaSkaitlis)){
+        return "Lūdzu ievadi skaitli nevis to!!!!!!!!!!!! >:("
+    }
+    if (cilvekaSkaitlis>datoraSkaitlis){
+        return "Skaitlis ir lielāks!"
+    };
+    if (cilvekaSkaitlis<datoraSkaitlis){
+        return "Skaitlis ir mazāks!"
+    };
+return "PAREIZI!!!!!!!!!!!!!!!!!!!!!!!!!!"
+}
+
+function izvadiAtbildi(atbilde){
+    document.getElementById("atbilde").innerHTML=atbilde;
 }
 
 function veiktParbaudi(){
     console.log("Pārbaudīts!!!", datoraSkaitlis);
+    let minejums = nolasitIevadito();
+    let rezultats = salidzinatSkaitlus(minejums);
+    izvadiAtbildi(rezultats);
 }
 
 function nomainitSkaitli(){

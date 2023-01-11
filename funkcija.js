32ddb5d41741f9643c37b4558b30b343127c5b33
kristiansabols <kristineabeltina79@gmail.com>
2023-01-11 08:36:17 +0200

    daudz

---
+++ funkcija.js
@@ -0,0 +1,13 @@
+//Jānolasa ievadītais
+//Jāizvēlas skaitlis datoram
+//Jāpārbauda ievadītais
+//Jāizvada atbilde
+
+function nolasitIevadito(){
+    let ievaditais = document.getElementById("ievade").value
+    console.log(ievaditais)
+}
+
+function veiktParbaudi(){
+    console.log("Pārbaudīts!!!")
+}
